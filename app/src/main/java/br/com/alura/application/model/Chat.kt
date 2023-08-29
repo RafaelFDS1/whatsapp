a29622f436d0908d166968b56b10f3340f1a0c4c
Rafael Ferraz de Santana <rafael_fds@outlook.com>
2023-08-29 14:01:55 -0300

    TODO upload

--- app/src/main/java/br/com/alura/application/model/Chat.kt
+++ app/src/main/java/br/com/alura/application/model/Chat.kt
@@ -4,9 +4,15 @@ import java.util.*
 
 class Chat(
 //    val image: String,
+    val userid: Long,
     var nome: String,
     var desc: String,
     val numero: String
 //    var lastContact: Date,
 ) {
 }
+
+// TODO: Entender como criar uma AppBar para o aplicativo;
+// TODO: Aplicar a criação da AppBar (Sem implementar a funcionalidade de Câmera do WhatsApp);
+// TODO: Criar a lógica de cada uma das telas do app;
+// TODO: Criar os ListViews de cada uma das sessões (Community, Status, Calls) Obs.: Caso o usuário não ter nenhuma comunidade, deve aparecer uma mensagem insentivando a criar.
